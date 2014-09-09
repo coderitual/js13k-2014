cfeb74c91f9282590e912566208db2ddb6bb04e5
skowronkow <skowronkow@gmail.com>
2014-09-09 02:25:41 +0200

    SAT Collision first commit

--- src/app.js
+++ src/app.js
@@ -1050,6 +1050,8 @@ EL.Body2d = function() {
     this.pos = vec2.create();
     this.acc = vec2.create();
     this.vec = vec2.create();
+    this.maxVec = vec2.create();
+    this.maxAcc = vec2.create();
 
     this.polygons = [];
 };
@@ -1064,6 +1066,126 @@ EL.Body2d.prototype.update = function(transform) {
         this.polygons[l].applyTransform(transform);
     }
 };
+
+EL.Contact2d = function() {
+    this.a = null;
+    this.b = null;
+    this.overlapN = vec2.create();
+    this.overlapV = vec2.create();
+    this.clear();
+};
+
+EL.Contact2d.prototype.clear = function() {
+    this.aInB = true;
+    this.bInA = true;
+    this.overlap = Number.MAX_VALUE;
+};
+
+function flattenPointsOn(points, normal, result) {
+    var min = Number.MAX_VALUE;
+    var max = -Number.MAX_VALUE;
+    var len = points.length;
+    for (var i = 0; i < len; i++ ) {
+        // The magnitude of the projection of the point onto the normal
+        var dot = points[i].dot(normal);
+        if (dot < min) { min = dot; }
+        if (dot > max) { max = dot; }
+    }
+    result[0] = min; result[1] = max;
+}
+
+function isSeparatingAxis(aPos, bPos, aPoints, bPoints, axis, response) {
+    var rangeA = T_ARRAYS.pop();
+    var rangeB = T_ARRAYS.pop();
+    // The magnitude of the offset between the two polygons
+    var offsetV = T_VECTORS.pop().copy(bPos).sub(aPos);
+    var projectedOffset = offsetV.dot(axis);
+    // Project the polygons onto the axis.
+    flattenPointsOn(aPoints, axis, rangeA);
+    flattenPointsOn(bPoints, axis, rangeB);
+    // Move B's range to its position relative to A.
+    rangeB[0] += projectedOffset;
+    rangeB[1] += projectedOffset;
+    // Check if there is a gap. If there is, this is a separating axis and we can stop
+    if (rangeA[0] > rangeB[1] || rangeB[0] > rangeA[1]) {
+        T_VECTORS.push(offsetV);
+        T_ARRAYS.push(rangeA);
+        T_ARRAYS.push(rangeB);
+        return true;
+    }
+    // This is not a separating axis. If we're calculating a response, calculate the overlap.
+    if (response) {
+        var overlap = 0;
+        // A starts further left than B
+        if (rangeA[0] < rangeB[0]) {
+            response['aInB'] = false;
+            // A ends before B does. We have to pull A out of B
+            if (rangeA[1] < rangeB[1]) {
+                overlap = rangeA[1] - rangeB[0];
+                response['bInA'] = false;
+                // B is fully inside A.  Pick the shortest way out.
+            } else {
+                var option1 = rangeA[1] - rangeB[0];
+                var option2 = rangeB[1] - rangeA[0];
+                overlap = option1 < option2 ? option1 : -option2;
+            }
+            // B starts further left than A
+        } else {
+            response['bInA'] = false;
+            // B ends before A ends. We have to push A out of B
+            if (rangeA[1] > rangeB[1]) {
+                overlap = rangeA[0] - rangeB[1];
+                response['aInB'] = false;
+                // A is fully inside B.  Pick the shortest way out.
+            } else {
+                var option1 = rangeA[1] - rangeB[0];
+                var option2 = rangeB[1] - rangeA[0];
+                overlap = option1 < option2 ? option1 : -option2;
+            }
+        }
+        // If this is the smallest amount of overlap we've seen so far, set it as the minimum overlap.
+        var absOverlap = Math.abs(overlap);
+        if (absOverlap < response['overlap']) {
+            response['overlap'] = absOverlap;
+            response['overlapN'].copy(axis);
+            if (overlap < 0) {
+                response['overlapN'].reverse();
+            }
+        }
+    }
+    T_VECTORS.push(offsetV);
+    T_ARRAYS.push(rangeA);
+    T_ARRAYS.push(rangeB);
+    return false;
+}
+
+function testPolygonPolygon(a, b, response) {
+    var aPoints = a['calcPoints'];
+    var aLen = aPoints.length;
+    var bPoints = b['calcPoints'];
+    var bLen = bPoints.length;
+    // If any of the edge normals of A is a separating axis, no intersection.
+    for (var i = 0; i < aLen; i++) {
+        if (isSeparatingAxis(a['pos'], b['pos'], aPoints, bPoints, a['normals'][i], response)) {
+            return false;
+        }
+    }
+    // If any of the edge normals of B is a separating axis, no intersection.
+    for (var i = 0;i < bLen; i++) {
+        if (isSeparatingAxis(a['pos'], b['pos'], aPoints, bPoints, b['normals'][i], response)) {
+            return false;
+        }
+    }
+    // Since none of the edge normals of A or B are a separating axis, there is an intersection
+    // and we've already calculated the smallest overlap (in isSeparatingAxis).  Calculate the
+    // final overlap vector.
+    if (response) {
+        response['a'] = a;
+        response['b'] = b;
+        response['overlapV'].copy(response['overlapN']).scale(response['overlap']);
+    }
+    return true;
+}
 
 /**
  * Game
